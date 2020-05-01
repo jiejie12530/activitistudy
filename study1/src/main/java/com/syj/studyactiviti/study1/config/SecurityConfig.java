69b896582193573b3bc5426fde705906eff91d42
sky <shiyunjie45@163.com>
2020-05-01 22:52:10 +0800

    【提交类型】： 初次提交 【问题描述】： 基本的配置,首先启动项目,这样可以增强信心 【需要测试】： 是

---
+++ study1/src/main/java/com/syj/studyactiviti/study1/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.syj.studyactiviti.study1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security.csrf().disable();
+    }
+}
